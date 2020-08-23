ae696adc069cbcd25201aecdb64556afac2b0379
Nibras M <nawaraaa2000@yahoo.com>
2020-08-23 11:00:20 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+/* When the input field receives input, convert the value from fahrenheit to kelvin */
+function temperatureConverter(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("outputKelvin").innerHTML=((valNum-32)/1.8)+273.15;
+  }
+
+async function getWeatherData(){
+    city = document.getElementsByName("city")[0].value;
+    console.log(city)
+    await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5998cc7be6bf85d82161ab58920ec663`)
+        .then(data => data.json())
+        .then(rawData => {
+            console.log(rawData)
+            document.getElementById("Temphigh").innerHTML=convertTemp(rawData.main.temp_max)
+            document.getElementById("Templow").innerHTML=convertTemp(rawData.main.temp_min)
+            document.getElementById("Forecast").innerHTML=rawData.weather[0].main
+            document.getElementById("Humidity").innerHTML=rawData.main.humidity
+        })
+        
+
+}
+function convertTemp(temp){
+    return Math.round(((temp-273.15) * 1.8) + 32)
+}
